ce42c35a50df7bf37da56415ba991444fd522b43
N-esquik <136918172+N-esquik@users.noreply.github.com>
2024-10-10 13:41:43 +0300

    Delete Scripts directory

--- Scripts/AnimatorData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public static class AnimatorData
-{
-    public static readonly int Speed = Animator.StringToHash(nameof(Speed));
-    public static readonly int DieEnemy = Animator.StringToHash(nameof(DieEnemy));
-    public static readonly int Hit = Animator.StringToHash(nameof(Hit));
-}
